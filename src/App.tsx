891f64b52ed915e8aa7b8b8d5e2d5df5eb4b9ea6
qirtudgus <qirtudgus@naver.com>
2023-03-29 18:36:21 +0900

    feat: vscode 확장프로그램 tailwindcss intellisense 테스트 완

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-3xl font-bold underline">Edit and save to reload.</p>
+        <p className="text-3xl font-bold underline-offset-2">
+          Edit and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
