2959df5749eafd1ff9aa63ba4358f39c54b17179
qirtudgus <qirtudgus@naver.com>
2023-03-31 19:38:46 +0900

    feat: 투두리스트 필터링 상태와 필터링된 리스트 상태 추가

--- src/@store/todoList.tsx
+++ src/@store/todoList.tsx
@@ -1,7 +1,29 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 import { TodoList } from "../@interface/TodoList";
 
 export const todoListState = atom<TodoList>({
   key: "todoListState",
   default: [{ id: 1, text: "리코일 써보기", isCompleted: false }],
+});
+
+export const todoListFilterState = atom({
+  key: "todoListFilterState",
+  default: "Show All",
+});
+
+export const filteredTodoListState = selector({
+  key: "filteredTodoListState",
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+
+    switch (filter) {
+      case "Show Completed":
+        return list.filter((item) => item.isCompleted);
+      case "Show Uncompleted":
+        return list.filter((item) => !item.isCompleted);
+      default:
+        return list;
+    }
+  },
 });
