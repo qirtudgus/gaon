1ddac70b419633455e292b41f4ae4b407de5e0fe
qirtudgus <qirtudgus@naver.com>
2023-03-31 18:30:34 +0900

    feat: 객체에 id와 완료여부 값 추가

--- src/@store/todoList.tsx
+++ src/@store/todoList.tsx
@@ -1,10 +1,12 @@
 import { atom } from "recoil";
 
 interface TodoList {
-  text?: string;
+  id: number;
+  text: string;
+  isCompleted: boolean;
 }
 
 export const todoListState = atom<TodoList[]>({
   key: "todoListState",
-  default: [{ text: "zzzz" }],
+  default: [{ id: 1, text: "리코일 써보기", isCompleted: false }],
 });
