3e543b84c4025580cbe12de3565712bbd75033ac
qirtudgus <qirtudgus@naver.com>
2023-03-31 19:40:01 +0900

    feat: 함수 작동원리 주석 추가

--- src/@containers/TodoList/TodoItem/index.tsx
+++ src/@containers/TodoList/TodoItem/index.tsx
@@ -4,6 +4,8 @@ import { todoListState } from "../../../@store/todoList";
 
 export const TodoItem = ({ id, text, isCompleted }: TodoItemInterface) => {
   const [todoList, setTodoList] = useRecoilState(todoListState);
+  //각 아이템의 index를 획득하는 변수다.
+  //todoList에서 props로 받아온 id와 같은 요소인 인덱스를 반환한다.
   const index = todoList.findIndex((listItem) => listItem.id === id);
 
   const editItemText = ({
@@ -45,15 +47,18 @@ export const TodoItem = ({ id, text, isCompleted }: TodoItemInterface) => {
     </div>
   );
 };
-
+//투두리스트중 선택한 요소의 value가 수정된 배열을 반환해주는 함수
 function replaceItemAtIndex(
   arr: TodoList,
   index: number,
   newValue: TodoItemInterface,
 ) {
+  console.log(...arr.slice(0, index));
+  console.log(...arr.slice(index + 1));
+  //현재 아이템 이전까지의 요소를 복사, 새로운 요소넣고, 현재 아이템 이후의 모든 요소 복사
   return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
 }
-
+//선택된 인덱스를 제외한 요소들을 복사하여 삭제 구현
 function removeItemAtIndex(arr: TodoList, index: number) {
   return [...arr.slice(0, index), ...arr.slice(index + 1)];
 }
