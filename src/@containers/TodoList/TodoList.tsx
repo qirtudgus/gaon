261d9fbf1c3709a802a368932b0700b5a60a5e45
qirtudgus <qirtudgus@naver.com>
2023-03-31 19:05:07 +0900

    feat: TodoItem 컴포넌트 적용

--- src/@containers/TodoList/TodoList.tsx
+++ src/@containers/TodoList/TodoList.tsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { todoListState } from "../@store/todoList";
+import { useRecoilState } from "recoil";
+import { todoListState } from "../../@store/todoList";
+import { TodoItem } from "./TodoItem";
 
 const TodoList = () => {
-  const todoList = useRecoilValue(todoListState);
-  const setTodoList = useSetRecoilState(todoListState);
+  // const todoList = useRecoilValue(todoListState);
+  // const setTodoList = useSetRecoilState(todoListState);
+
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+
   const [value, setValue] = useState("");
 
   const addItem = () => {
     setTodoList((prev) => [
       ...prev,
       {
+        id: prev[prev.length - 1].id + 1,
         text: value,
+        isCompleted: false,
       },
     ]);
     setValue("");
@@ -25,9 +31,11 @@ const TodoList = () => {
     <div>
       <input onChange={onChange} value={value}></input>
       <button onClick={addItem}>더하기</button>
-      {todoList.map((el, index) => (
-        <p key={index}>{el?.text}</p>
-      ))}
+      {todoList.map((el, index) => {
+        return (
+          <TodoItem id={el.id} isCompleted={el.isCompleted} text={el.text} />
+        );
+      })}
     </div>
   );
 };
