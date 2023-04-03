67a46ab54d8649d7e44e94da7e5cecfd7a661d8f
qirtudgus <qirtudgus@naver.com>
2023-04-04 01:49:42 +0900

    feat: selector를 이용한 데이터 핸들링

--- src/@store/todoList.tsx
+++ src/@store/todoList.tsx
@@ -1,29 +1,49 @@
-import { atom, selector } from "recoil";
-import { TodoList } from "../@interface/TodoList";
+import { atom, selector } from 'recoil';
+import { TodoList } from '../@interface/TodoList';
 
 export const todoListState = atom<TodoList>({
-  key: "todoListState",
-  default: [{ id: 1, text: "리코일 써보기", isCompleted: false }],
+  key: 'todoListState',
+  default: [{ id: 1, text: '리코일 써보기', isCompleted: false }],
 });
 
 export const todoListFilterState = atom({
-  key: "todoListFilterState",
-  default: "Show All",
+  key: 'todoListFilterState',
+  default: 'Show All',
 });
 
 export const filteredTodoListState = selector({
-  key: "filteredTodoListState",
+  key: 'filteredTodoListState',
   get: ({ get }) => {
     const filter = get(todoListFilterState);
     const list = get(todoListState);
 
     switch (filter) {
-      case "Show Completed":
+      case 'Show Completed':
         return list.filter((item) => item.isCompleted);
-      case "Show Uncompleted":
+      case 'Show Uncompleted':
         return list.filter((item) => !item.isCompleted);
       default:
         return list;
     }
+  },
+});
+
+export const todoListStatsState = selector({
+  key: 'todoListStatsState',
+  get: ({ get }) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter(
+      (item) => item.isCompleted,
+    ).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum;
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted,
+    };
   },
 });
