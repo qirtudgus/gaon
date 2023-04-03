6f731e445167e0c58ae64970b5c9fa9f95770b05
qirtudgus <qirtudgus@naver.com>
2023-04-04 03:53:40 +0900

    feat: 리코일 비동기쿼리 아톰 핸들링

--- src/@containers/TodoList/Api/Api.tsx
+++ src/@containers/TodoList/Api/Api.tsx
@@ -1,18 +1,31 @@
 import { useEffect, useRef } from 'react';
-import { useRecoilValue } from 'recoil';
-import { ApiState } from '../../../@store/api';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { ApiState, ChangePossibleApiState } from '../../../@store/api';
 
 const Api = () => {
-  const postList = useRecoilValue(ApiState);
+  const [postList, setPostList] = useRecoilState(ChangePossibleApiState);
 
-  if (!postList || !postList.data) {
-    // postList가 undefined이거나 data 속성이 없는 경우
-    return null;
-  }
+  useEffect(() => {
+    // 오류없이 아톰의 초기값을 핸들링할 수 있다.
+    setPostList((prev: any) => prev.concat({ id: '1' }));
+  }, []);
+
+  //   if (!postList || !postList) {
+  //     // postList가 undefined이거나 data 속성이 없는 경우
+  //     return null;
+  //   }
 
   return (
     <div>
-      {postList.data.map((el: any, idx: number) => (
+      <button
+        onClick={() => {
+          console.log(postList);
+          setPostList((prev: any) => prev.concat({ id: '1' }));
+        }}
+      >
+        포스트확인
+      </button>
+      {postList.map((el: any, idx: number) => (
         <p key={idx}>{el.id}</p>
       ))}
     </div>
