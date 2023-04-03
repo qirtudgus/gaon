c165a3b0dfa89fea98aaacab532de53e85a4fd6f
qirtudgus <qirtudgus@naver.com>
2023-04-04 02:16:20 +0900

    feat: 리코일 비동기 resolve는 React.Suspense와 함께 동작한다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import TodoList from "./@containers/TodoList/TodoList";
-import "./App.css";
+import React from 'react';
+import Api from './@containers/TodoList/Api/Api';
+import TodoList from './@containers/TodoList/TodoList';
+
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="text-3xl font-bold text-blue-500">
+    <div className='App'>
+      <header className='App-header'>
+        <p className='text-3xl font-bold text-blue-500'>
           Edit and save to reload.
         </p>
-        <p className="text-3xl font-bold text-blue-100 bg-white text-green-900 hover:text-red-900 hover:-translate-y-[500px] duration-[3000ms] py-10">
+        <p className='text-3xl font-bold text-blue-100 bg-white text-green-900 hover:text-red-900 hover:-translate-y-[500px] duration-[3000ms] py-10'>
           Edit and save to reload.
         </p>
       </header>
       <TodoList />
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Api />
+      </React.Suspense>
     </div>
   );
 }
