3481fb3896c4acc538f133169a689ba9c5e7da92
qirtudgus <qirtudgus@naver.com>
2023-04-04 01:20:32 +0900

    feat: 태그 조정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
+import TodoList from "./@containers/TodoList/TodoList";
 import "./App.css";
-import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p className="text-3xl font-bold text-blue-500">
           Edit and save to reload.
         </p>
-        <p className="text-3xl font-bold text-blue-100 bg-white text-green-900 hover:text-red-900 hover:-translate-y-10 duration-[3000ms]">
+        <p className="text-3xl font-bold text-blue-100 bg-white text-green-900 hover:text-red-900 hover:-translate-y-[500px] duration-[3000ms] py-10">
           Edit and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <TodoList />
     </div>
   );
 }
