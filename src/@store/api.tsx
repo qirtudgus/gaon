a988b20e9edcef1d487271e6d945237705980721
qirtudgus <qirtudgus@naver.com>
2023-04-06 03:16:20 +0900

    feat: PostAPI 타입 지정

--- src/@store/api.tsx
+++ src/@store/api.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { atom, selector } from 'recoil';
+import { PostListInterface } from '../@interface/TodoList';
 
 export const ApiState = selector({
   key: 'ApiState',
@@ -13,17 +14,20 @@ export const ApiState = selector({
 아톰의 기본값을 비동기쿼리 값으로 설정하여
 해당값을 클라이언트에서 핸들링할 수 있게된다.
 */
-export const ChangePossibleApiState = atom({
+export const ChangePossibleApiState = atom<PostListInterface[]>({
   key: 'ChangePossibleApiState',
   default: selector({
     key: 'ChangeApiState',
     get: async ({ get }) => {
       const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      return res.data;
+      return res.data.slice(0, 10);
     },
   }),
 });
 
+/*
+React.suspense와 ErrorBoundary로 진행상태를 핸들링해줄 수 있다.
+*/
 export const ErrorApiState = selector<any | unknown>({
   key: 'ErrorApiState',
   get: async ({ get }) => {
