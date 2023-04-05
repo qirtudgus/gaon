9b3dcdfd01d171500c2d6105305717b7bc4e1ccb
qirtudgus <qirtudgus@naver.com>
2023-04-06 05:05:17 +0900

    feat: 리액트쿼리 클라이언트 생성

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
-import ReactDOM from "react-dom/client";
-import { RecoilRoot } from "recoil";
-import App from "./App";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+// Create a client
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <RecoilRoot>
-    <App />
-  </RecoilRoot>,
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </QueryClientProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
