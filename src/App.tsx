0a61242f90041c0c2bc757490d728ea7bfa64a46
qirtudgus <qirtudgus@naver.com>
2023-04-23 19:10:05 +0900

    feat: 커밋 4 push 후 branch

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,8 @@ function App() {
       <header className=' w-full h-[50px] bg-yellow-500'></header>
       <header className=' w-full h-[50px] bg-yellow-500'></header>
       <header className=' w-full h-[50px] bg-yellow-500'></header>
+      <header className=' w-full h-[50px] bg-yellow-500'></header>
+      <header className=' w-full h-[50px] bg-yellow-500'></header>
       <MyInput ref={ref} />
       <button
         onClick={() => {
