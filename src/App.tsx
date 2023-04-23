b8aa0f864002e284a093f0dfd8952ae34a6d9f45
qirtudgus <qirtudgus@naver.com>
2023-04-23 19:15:48 +0900

    feat: 커밋 5

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,10 @@ function App() {
       <header className=' w-full h-[50px] bg-yellow-500'></header>
       <header className=' w-full h-[50px] bg-yellow-500'></header>
       <header className=' w-full h-[50px] bg-yellow-500'></header>
+      <header className=' w-full h-[50px] bg-yellow-500'></header>
+      <header className=' w-full h-[50px] bg-yellow-500'></header>
+      <header className=' w-full h-[50px] bg-yellow-500'></header>
+      <header className=' w-full h-[50px] bg-yellow-500'></header>
       <MyInput ref={ref} />
       <button
         onClick={() => {
